43853fd57a3dd7e513c2a22588f79e678131caa3
Fahad Iqbal Zafar <67633549+FahadIqbal12@users.noreply.github.com>
2022-01-19 18:50:59 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask,jsonify,request
+from data import data
+
+app = Flask(__name__)
+@app.route("/stars")
+
+def stars():
+    name = request.args.get('name')
+    star_data = next(item for item in data if item["name"] == name)
+    return jsonify({
+        "data":star_data,
+        "message":"success"
+    }),200
+
+if __name__ == "__main__":
+    app.run()
